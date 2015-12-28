d44bb4dc821d80d66f015e3b04ac7fb9ce78f8e0
Roberto <roberto@pcroberto>
2015-12-28 16:43:11 -0200

    initial commit

---
+++ sizes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	cout << "Chara " << sizeof(char) <<'\n';
+	cout << "short" << sizeof(short) << '\n';
+	cout << "int " << sizeof(int) << '\n';
+	cout << "long " << sizeof(long) << '\n';
+	cout << "float " << sizeof(float) << '\n';
+	cout << "Double" << sizeof(double) << '\n';
+
+}
